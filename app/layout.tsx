729a7b2376179ef978df886b403b6cb8f3f70995
pat <pat.emery21+pocketscribe@gmail.com>
2023-10-15 23:14:19 -0400

    NextJS Template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import NextThemeProvider from '../components/NextThemeProvider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NextThemeProvider>
+        {children}
+      </NextThemeProvider>  
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,20 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    './providers/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: "class",
   theme: {
+    screens: {
+      mob: "375px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+      laptopl: "1440px",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
